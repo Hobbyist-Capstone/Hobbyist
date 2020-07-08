d9a62c44e8df599431ed60d26b7582a896fc25d5
Valeria Meza <valeriameza173@gmail.com>
2020-07-07 20:25:43 -0500

    completed the basic setup.

---
+++ src/main/java/com/hobbyist/hobbyist/HobbyistApplication.java
@@ -0,0 +1,13 @@
+package com.hobbyist.hobbyist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HobbyistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hobbyist/hobbyist/HobbyistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobbyist.hobbyist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
