3c31d92d5a54d8089532ab0eade1c6f531b07790
Valeria Meza <valeriameza173@gmail.com>
2020-07-10 13:57:35 -0500

    db working and controller connected to view.

--- src/main/java/com/hobbyist/hobbyist/controllers/HobbyController.java
+++ src/main/java/com/hobbyist/hobbyist/controllers/HobbyController.java
@@ -1,21 +1,24 @@
 package com.hobbyist.hobbyist.controllers;
 
+import com.hobbyist.hobbyist.models.User;
+import com.hobbyist.hobbyist.repos.HobbyRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HobbyController {
 
-//    private HobbyRepository hobbyDao;
-//
-//    public HobbyController(HobbyRepository hobbyDao) {
-//        this.hobbyDao = hobbyDao;
-//    }
+    private HobbyRepository hobbyDao;
+
+    public HobbyController(HobbyRepository hobbyDao) {
+        this.hobbyDao = hobbyDao;
+    }
 
     @GetMapping("/hobby")
-    @ResponseBody
-    public String index() {
-        return "Showing hobby";
+    public String index(Model model) {
+        model.addAttribute("hobby", hobbyDao.findAll());
+        return "hobby/hobbyView";
     }
 }
