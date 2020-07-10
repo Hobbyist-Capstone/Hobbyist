7d4294a33ea647c4e718d3d505c50fef577e2526
Valeria Meza <valeriameza173@gmail.com>
2020-07-10 11:42:01 -0500

    fixed db issue.

--- src/main/java/com/hobbyist/hobbyist/repos/HobbyStatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hobbyist.hobbyist.repos;
-
-import com.hobbyist.hobbyist.models.HobbyStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HobbyStatusRepository extends JpaRepository<HobbyStatus, Long> {
-}
