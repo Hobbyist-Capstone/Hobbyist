b3d033f279e444a623fb8e535125e28f4c27028b
Valeria Meza <valeriameza173@gmail.com>
2020-07-11 14:43:14 -0500

    testing hobbies single view and all hobbies view.

--- src/main/java/com/hobbyist/hobbyist/controllers/HobbyController.java
+++ src/main/java/com/hobbyist/hobbyist/controllers/HobbyController.java
@@ -2,6 +2,7 @@ package com.hobbyist.hobbyist.controllers;
 
 import com.hobbyist.hobbyist.models.User;
 import com.hobbyist.hobbyist.repos.HobbyRepository;
+import com.hobbyist.hobbyist.repos.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +12,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HobbyController {
 
     private HobbyRepository hobbyDao;
+    private UserRepository userDao;
+
+    public HobbyController () {}
+
+    public HobbyController(UserRepository userDao) {
+        this.userDao = userDao;
+    }
 
     public HobbyController(HobbyRepository hobbyDao) {
         this.hobbyDao = hobbyDao;
     }
 
+    // displays a single hobby
     @GetMapping("/hobby")
-    public String index(Model model) {
-        model.addAttribute("hobby", hobbyDao.findAll());
+    public String singleHobby(Model model) {
+        model.addAttribute("hobby", hobbyDao.getOne(2L));
+        model.addAttribute("status", userDao.getOne(2L));
         return "hobby/hobbyView";
+    }
+
+    // displays all hobbies
+    @GetMapping("/hobbies")
+    public String index(Model model) {
+        model.addAttribute("hobbies", hobbyDao.findAll());
+        model.addAttribute("status", userDao.findAll());
+        return "hobbies/allHobbiesView";
     }
 }
