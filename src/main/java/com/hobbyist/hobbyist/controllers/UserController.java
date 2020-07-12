8f8d2117d67bb217d8d0417dc02ba06dc3c60390
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-07-12 08:48:30 -0500

    feat : front-end edit button in profile only shows up for relevant user

--- src/main/java/com/hobbyist/hobbyist/controllers/UserController.java
+++ src/main/java/com/hobbyist/hobbyist/controllers/UserController.java
@@ -58,9 +58,16 @@ public class UserController {
 
     @GetMapping("/profile/{username}")
     public String showProfile(@PathVariable String username, Model vModel) {
+
+        vModel.addAttribute("user", userDao.findByUsername(username));
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        vModel.addAttribute("userName", user.getUsername());
+
+
         //user must be logged in to view this page
-        User currentUser = userDao.findByUsername(username);
-        vModel.addAttribute("user", currentUser);
+//        User currentUser = userDao.findByUsername(username);
+//        vModel.addAttribute("user", currentUser);
+//        vModel.addAttribute("userName", currentUser.getUsername());
 //        if(logUser == null){
 //            vModel.addAttribute("msg", "Please log in");
 //            return"error/custom";
@@ -75,6 +82,7 @@ public class UserController {
     @GetMapping("users/{id}/edit")
     public String showEditProfile(@PathVariable long id, Model vModel) {
         User user = userDao.getOne(id);
+//        vModel.addAttribute("user", userDao.findById(id));
         vModel.addAttribute("user", user);
 //        vModel.addAttribute("showEditControls", userService.canEditProfile(user));
         return "users/edit";
@@ -87,6 +95,6 @@ public class UserController {
         userToEdit.setId(id);
         userToEdit.setPassword(passwordEncoder.encode(userToEdit.getPassword()));
         userDao.save(userToEdit);
-        return "redirect:/profile/";
+        return "redirect:/profile/" + userToEdit.getUsername();
     }
 }
