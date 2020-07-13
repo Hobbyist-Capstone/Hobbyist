b7ccbbab61c0390d1ffeccc36ea67d3701bb3843
Moriah Humphries <moriah.humphries@gmail.com>
2020-07-13 16:15:32 -0500

    added additional files for css, updated html, included materialize

--- src/main/java/com/hobbyist/hobbyist/controllers/IndexController.java
+++ src/main/java/com/hobbyist/hobbyist/controllers/IndexController.java
@@ -1,21 +1,9 @@
 package com.hobbyist.hobbyist.controllers;
 
-import com.hobbyist.hobbyist.models.User;
-import com.hobbyist.hobbyist.repos.UserRepository;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import com.hobbyist.hobbyist.models.Hobby;
 import com.hobbyist.hobbyist.repos.HobbyRepository;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @Controller
 public class IndexController {
@@ -36,7 +24,7 @@ public class IndexController {
         model.addAttribute("c1", hobbyDao.filterByCategory(c1));
         model.addAttribute("c2", hobbyDao.filterByCategory(c2));
         model.addAttribute("c3", hobbyDao.filterByCategory(c3));
-        return "index";
+        return "index/index";
     }
 
 //    future search feature
