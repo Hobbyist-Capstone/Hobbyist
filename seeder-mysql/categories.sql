bc9d24e6a6c554accc111114690854215a4723e8
Valeria Meza <valeriameza173@gmail.com>
2020-07-14 11:48:43 -0500

    production db seeder files.

---
+++ seeder-mysql/categories.sql
@@ -0,0 +1,7 @@
+use hobbyist_db;
+
+insert into categories (name)
+VALUES ('Sports'),
+       ('Crafts'),
+       ('Technology'),
+       ('Education')
--- seeder-mysql/users.sql
+++ seeder-mysql/users.sql
@@ -1,9 +1,9 @@
 use hobbyist_db;
 
-INSERT INTO  users(email, first_name, is_admin, last_name, password, username) VALUES
-('Moriah@email.com', 'Moriah', true, 'lastname', '1234', 'Moriah' ),
-('Jacob@email.com', 'Jacob', true, 'lastname', '1234', 'Jacob' ),
-('Valeria@email.com', 'Valeria', true, 'lastname', '1234', 'Valeria' ),
-('Nayoung@email.com', 'Nayoung', true, 'lastname', '1234', 'Nayoung' );
+INSERT INTO users(email, first_name, is_admin, last_name, password, username)
+VALUES ('Moriah@email.com', 'Moriah', true, 'lastname', '1234', 'Moriah'),
+       ('Jacob@email.com', 'Jacob', true, 'lastname', '1234', 'Jacob'),
+       ('Valeria@email.com', 'Valeria', true, 'lastname', '1234', 'Valeria'),
+       ('Nayoung@email.com', 'Nayoung', true, 'lastname', '1234', 'Nayoung');
 
 
