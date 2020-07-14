d84aad645aa129213fe31a1c9faf0e402cdaeca5
Valeria Meza <valeriameza173@gmail.com>
2020-07-14 15:05:35 -0500

    search engine is working.

--- src/main/java/com/hobbyist/hobbyist/controllers/HobbyController.java
+++ src/main/java/com/hobbyist/hobbyist/controllers/HobbyController.java
@@ -1,5 +1,6 @@
 package com.hobbyist.hobbyist.controllers;
 
+import com.hobbyist.hobbyist.models.Hobby;
 import com.hobbyist.hobbyist.models.User;
 import com.hobbyist.hobbyist.models.UserHobby;
 import com.hobbyist.hobbyist.repos.HobbyRepository;
@@ -10,7 +11,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class HobbyController {
@@ -33,6 +37,13 @@ public class HobbyController {
     @GetMapping("/hobbies")
     public String allHobbies(Model model) {
         model.addAttribute("hobbies", hobbyDao.findAll());
+        return "hobbies/allHobbiesView";
+    }
+
+    @GetMapping("/search")
+    public String searchResults(Model model, @RequestParam(name = "search") String search) {
+        List<Hobby> hobbies = hobbyDao.searchByTitle(search);
+        model.addAttribute("hobbies", hobbies);
         return "hobbies/allHobbiesView";
     }
 }
