2da5be436f3cda954d6812f9e75443168ae87c2b
Valeria Meza <valeriameza173@gmail.com>
2020-07-14 10:24:27 -0500

    testing

--- src/main/java/com/hobbyist/hobbyist/repos/HobbyRepository.java
+++ src/main/java/com/hobbyist/hobbyist/repos/HobbyRepository.java
@@ -4,4 +4,5 @@ import com.hobbyist.hobbyist.models.Hobby;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HobbyRepository extends JpaRepository<Hobby, Long> {
+
 }
