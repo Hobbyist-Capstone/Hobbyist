50b9cf98e934402a83b56c12ea965ef1ea8bfeec
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-07-17 11:49:30 -0500

    navbar

--- src/main/java/com/hobbyist/hobbyist/controllers/IndexController.java
+++ src/main/java/com/hobbyist/hobbyist/controllers/IndexController.java
@@ -1,7 +1,11 @@
 package com.hobbyist.hobbyist.controllers;
 
 
+import com.hobbyist.hobbyist.models.User;
 import com.hobbyist.hobbyist.repos.CategoryRepository;
+import com.hobbyist.hobbyist.repos.UserHobbyRepository;
+import com.hobbyist.hobbyist.repos.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,16 +17,27 @@ public class IndexController {
 
     private CategoryRepository categoryDao;
     private HobbyRepository hobbyDao;
+    private UserHobbyRepository userHobbyDao;
+    private UserRepository  userDao;
 
 
-    public IndexController(HobbyRepository hobbyDao, CategoryRepository categoryDao) {
+
+    public IndexController(HobbyRepository hobbyDao, CategoryRepository categoryDao, UserHobbyRepository userHobbyDao, UserRepository  userDao
+    ) {
         this.hobbyDao = hobbyDao;
         this.categoryDao = categoryDao;
+        this.userHobbyDao = userHobbyDao;
+        this.userDao = userDao;
+
     }
 
     @GetMapping("/")
-    public String index(Model model) {
-//        model.addAttribute("category", categoryDao.findAll());
+    public String index(Model model, String username) {
+
+
+//        User user = userDao.findByUsername(username);
+
+//        model.addAttribute("user", user);
         model.addAttribute("hobbies", hobbyDao.findAll());
         return "index/index";
     }
