49de1e768dd5f7811f68fabc47a9f0da8ce26dbb
nayokim <nayoung.kimpak@gmail.com>
2020-07-18 08:35:55 -0500

    add followers and links

--- src/main/java/com/hobbyist/hobbyist/controllers/FriendController.java
+++ src/main/java/com/hobbyist/hobbyist/controllers/FriendController.java
@@ -9,27 +9,34 @@ import com.hobbyist.hobbyist.repos.UserHobbyRepository;
 import com.hobbyist.hobbyist.repos.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@Controller
 public class FriendController {
 
     private UserRepository userDao;
     private FriendListRepository friendListDao;
 
-    public FriendController(UserRepository userDao,FriendListRepository  friendListDao) {
+    public FriendController(UserRepository userDao,FriendListRepository friendListDao) {
         this.userDao = userDao;
         this.friendListDao = friendListDao;
-
     }
 
-    @PostMapping("/users/{username}/friend-request")
-    public String sendFriendRequest(@PathVariable  String username, @ModelAttribute long userId) {
+    @GetMapping("/profile/friend-request")
+    public String sendFriendRequest(@RequestParam long userId) {
         User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        FriendList friend = friendListDao.save(new FriendList(currentUser, userDao.getOne(userId), FriendStatus.PENDING));
-        return "redirect:/profile/";
+        User userInDb = userDao.getOne(currentUser.getId());
+        User user = userDao.getOne(userId);
+        FriendList friend = new FriendList(userInDb, user, FriendStatus.ACCEPTED);
+        friendListDao.save(friend);
+        return "redirect:/profile";
     }
 
 }
--- src/main/java/com/hobbyist/hobbyist/repos/FriendListRepository.java
+++ src/main/java/com/hobbyist/hobbyist/repos/FriendListRepository.java
@@ -2,9 +2,11 @@ package com.hobbyist.hobbyist.repos;
 
 import com.hobbyist.hobbyist.models.FriendList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
+@Transactional(propagation = Propagation.REQUIRED)
 public interface FriendListRepository extends JpaRepository <FriendList, Long> {
 //    List<FriendList> findAllByStatus(String status);
 
