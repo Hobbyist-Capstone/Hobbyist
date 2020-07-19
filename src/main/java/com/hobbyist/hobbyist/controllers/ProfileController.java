f400768d41752bce6aba2e25a97c6656b82e7203
nayokim <nayoung.kimpak@gmail.com>
2020-07-19 15:32:05 -0500

    edit button only on logged-in users profile page

--- src/main/java/com/hobbyist/hobbyist/controllers/ProfileController.java
+++ src/main/java/com/hobbyist/hobbyist/controllers/ProfileController.java
@@ -1,21 +1,16 @@
 package com.hobbyist.hobbyist.controllers;
 
+import com.hobbyist.hobbyist.models.FriendList;
 import com.hobbyist.hobbyist.models.Hobby;
 import com.hobbyist.hobbyist.models.User;
 import com.hobbyist.hobbyist.models.UserHobby;
-import com.hobbyist.hobbyist.repos.CategoryRepository;
-import com.hobbyist.hobbyist.repos.UserHobbyRepository;
-import com.hobbyist.hobbyist.repos.UserRepository;
+import com.hobbyist.hobbyist.repos.*;
 import com.hobbyist.hobbyist.services.UserService;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import com.hobbyist.hobbyist.repos.HobbyRepository;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,20 +26,25 @@ public class ProfileController {
     private PasswordEncoder passwordEncoder;
     private UserHobbyRepository userHobbyDao;
     private HobbyRepository hobbyDao;
+    private FriendListRepository friendListDao;
+
 
 
     private UserService userService;
 
-    public ProfileController(UserRepository userDao, PasswordEncoder passwordEncoder, UserHobbyRepository userHobbyDao, HobbyRepository hobbyDao) {
+    public ProfileController(UserRepository userDao, PasswordEncoder passwordEncoder, UserHobbyRepository userHobbyDao, HobbyRepository hobbyDao, FriendListRepository friendListDao) {
         this.userDao = userDao;
         this.passwordEncoder = passwordEncoder;
         this.userHobbyDao = userHobbyDao;
         this.hobbyDao = hobbyDao;
+        this.friendListDao = friendListDao;
     }
+
+
 
     //user logged in profile
     @GetMapping("/profile")
-    public String showProfile(Model vModel) {
+    public String showProfile( Model vModel) {
 
         User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User userInDb = userDao.getOne(currentUser.getId());
@@ -55,9 +55,9 @@ public class ProfileController {
         vModel.addAttribute("friendsList", userInDb.getFriends());
 
 //        vModel.addAttribute("userNameHobby", currentUser.getUsername());
-//        vModel.addAttribute("user", currentUser.getUsername());
+        vModel.addAttribute("currentUsername", currentUser.getUsername());
 
-        List<Hobby> hobby = hobbyDao.findAll();
+        List <Hobby> hobby = hobbyDao.findAll();
         vModel.addAttribute("hobbies", hobby);
 
         return "users/profile-view";
@@ -75,19 +75,27 @@ public class ProfileController {
         vModel.addAttribute("user", userDao.findByUsername(username));
         vModel.addAttribute("user", user);
         vModel.addAttribute("userName", currentUser.getUsername());
-
         List <Hobby> hobby = hobbyDao.findAll();
         vModel.addAttribute("hobbies", hobby);
         return "users/profile-view";
     }
 
+//
+//    @GetMapping("/profile/{username}")
+//    public String showUsersProfile(Model vModel, @PathVariable String username) {
+//        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        User userInDb = userDao.getOne(currentUser.getId());
+//        vModel.addAttribute("user", userInDb);
+//        vModel.addAttribute("friendsList", userInDb.getFriends());
+//        vModel.addAttribute("user", userDao.findByUsername(username));
+//        vModel.addAttribute("userName", userInDb.getUsername());
+//        return "users/profile-view";
+//    }
 
     @GetMapping("users/{id}/edit")
     public String showEditProfile(@PathVariable long id, Model vModel) {
         User user = userDao.getOne(id);
-//        vModel.addAttribute("user", userDao.findById(id));
         vModel.addAttribute("user", user);
-//        vModel.addAttribute("showEditControls", userService.canEditProfile(user));
         return "users/edit";
     }
 
@@ -99,4 +107,23 @@ public class ProfileController {
         userDao.save(userToEdit);
         return "redirect:/profile/" + userToEdit.getUsername();
     }
+
+//    @GetMapping("users/hobby")
+//    public String showCreatedHobbies(Model model){
+//
+//
+//
+//        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        User userInDb = userDao.getOne(currentUser.getId());
+//        model.addAttribute("userNameHobby", currentUser.getUsername());
+//        model.addAttribute("user", currentUser.getUsername());
+//
+//
+//        List <Hobby> hobby = hobbyDao.findAll();
+//        model.addAttribute("hobbies", hobby);
+//
+//        return "users/profile";
+//    }
+
+
 }
