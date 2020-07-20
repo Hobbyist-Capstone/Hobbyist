cb80f288ca13834e977d96c8ae3d5bf0e92efc98
nayokim <nayoung.kimpak@gmail.com>
2020-07-20 11:46:38 -0500

    user created hobbies

--- src/main/java/com/hobbyist/hobbyist/controllers/AdminController.java
+++ src/main/java/com/hobbyist/hobbyist/controllers/AdminController.java
@@ -1,11 +1,13 @@
 package com.hobbyist.hobbyist.controllers;
 
 import com.hobbyist.hobbyist.models.User;
+import com.hobbyist.hobbyist.repos.HobbyRepository;
 import com.hobbyist.hobbyist.repos.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,16 +15,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AdminController {
 
     private UserRepository usersDao;
+    private HobbyRepository hobbyDao;
 
-    public AdminController(UserRepository usersDao) {
+    public AdminController(UserRepository usersDao, HobbyRepository hobbyDao) {
         this.usersDao = usersDao;
+        this.hobbyDao = hobbyDao;
     }
 
     @GetMapping("/admin")
     public String siteAdmin(Model model) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (user.isAdmin()) {
-            model.addAttribute("user", usersDao.findAll());
+            model.addAttribute("users", usersDao.findAll());
+            model.addAttribute("hobbies", hobbyDao.findAll());
             return "admin/admin";
         } else {
             return "registration/login";
