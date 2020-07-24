01c7336abcc7b694951f014a67ea928cee86298b
nayokim <nayoung.kimpak@gmail.com>
2020-07-24 09:04:51 -0500

    redirect and profile view tests

--- src/test/java/com/hobbyist/hobbyist/UserHobbyistIntegrationTest.java
+++ src/test/java/com/hobbyist/hobbyist/UserHobbyistIntegrationTest.java
@@ -12,17 +12,24 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
 
 import static org.junit.Assert.assertNotNull;
 
 import javax.servlet.http.HttpSession;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = HobbyistApplication.class)
@@ -87,11 +94,34 @@ public class UserHobbyistIntegrationTest {
     public void testCreateUser() throws Exception {
         // Makes a Post request to /ads/create and expect a redirection to the Ad
         this.mvc.perform(
-                post("/ads/create").with(csrf())
+                post("/login").with(csrf())
                         .session((MockHttpSession) httpSession)
                         // Add all the required parameters to your request like this
-                        .param("title", "test")
-                        .param("description", "for sale"))
+                        .param("firstName", "firstName")
+                        .param("lastName", "lastName")
+                        .param("userName", "testUser")
+                        .param("password", "Testing12345")
+                        .param("email", "email@email.com"))
+
                 .andExpect(status().is3xxRedirection());
     }
+
+    @Test
+    public void testRedirectProfile() throws Exception {
+        // Makes a Get request to /profile and expect a redirection to the login page
+        this.mvc.perform(get("/profile" ))
+                .andExpect(status().is(302));
+    }
+
+
+    @Test
+    @WithMockUser(username="testUser", password="Testing12345")
+    public void testShowProfile() throws Exception{
+        //test that user can login and be directed to profile
+        this.mvc.perform(get("/profile"))
+                .andExpect(status().isOk());
+    }
+
+
+
 }
