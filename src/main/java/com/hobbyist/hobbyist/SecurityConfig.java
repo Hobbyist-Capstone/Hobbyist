0b256423cb6b7aded1883a5e5888cc1ea24de9f8
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-07-26 18:41:44 -0500

    redirection

--- src/main/java/com/hobbyist/hobbyist/SecurityConfig.java
+++ src/main/java/com/hobbyist/hobbyist/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 /* Login configuration */
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/") // user's home page, it can be any URL
+                .defaultSuccessUrl("/profile") // user's home page, it can be any URL
                 .permitAll() // Anyone can go to the login page
                 /* Logout configuration */
                 .and()
